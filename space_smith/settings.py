7e1b579eaf3944bf5dcb206b4e5ebaae906746f9
Burhana786 <119918194+Burhana786@users.noreply.github.com>
2026-08-14 12:34:02 +0530

    update blog headind style

--- space_smith/settings.py
+++ space_smith/settings.py
@@ -215,6 +215,14 @@ CKEDITOR_5_CONFIGS = {
                 'undo', 'redo',
             ]
         },
+        'heading': {
+            'options': [
+                { 'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph' },
+                { 'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2' },
+                { 'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3' },
+                { 'model': 'heading4', 'view': 'h4', 'title': 'Heading 4', 'class': 'ck-heading_heading4' },
+            ]
+        },
         'image': {
             'toolbar': [
                 'imageTextAlternative', 'toggleImageCaption',
